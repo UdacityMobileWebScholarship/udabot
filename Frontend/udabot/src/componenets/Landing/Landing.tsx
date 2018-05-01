ae48b9a172d21aea1dae93d3d97701fada6bce82
Biboswan Roy <biboswan98@gmail.com>
2018-05-01 20:24:24 +0530

    Used Background component

--- Frontend/udabot/src/componenets/Landing/Landing.tsx
+++ Frontend/udabot/src/componenets/Landing/Landing.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import {Link} from 'react-router-dom';
 import CustomButton from '../CustomButton/CustomButton';
+import Background from '../Background/Background';
 import './Landing.css';
+
 class Landing extends React.Component {
     public render() {
         return (
-            <div className="bg">
+            <Background>
             <div className="heading-font">
             <p className="big">Hi! <br/>
             Welcome To Udacity PathFinder</p>
@@ -13,7 +15,7 @@ class Landing extends React.Component {
             Tap the button below to start chatting</p>
             <Link to="/chat"><CustomButton label='Start Chatting'/></Link>
             </div>
-            </div>
+            </Background>
         );
     }
 }
